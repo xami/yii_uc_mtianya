fe27bf6d0d1bf2d5974c5e846e8594fa88dfa932
xami <xami520@gmail.com>
2012-10-04 23:04:14 +0800

    del uc home bbs

--- bbs/data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 4b111f423ac411406a4614d0dd7d30fa
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- bbs/data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 3943ffd4b9ffe68a733d2ccd39ed43f8
-
-$pluginsetting = array (
-);
-?>
