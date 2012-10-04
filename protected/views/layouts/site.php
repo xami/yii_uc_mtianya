9fb3eeb2ef6ec330aef2787c5140d9b9bafd9bcb
xami <xami520@gmail.com>
2012-10-04 21:59:16 +0800

    css

--- protected/views/layouts/site.php
+++ protected/views/layouts/site.php
@@ -3,8 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/yiiplayground/css/main.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/user.css" />
 
     <meta name='robots' content='noindex,nofollow' />
